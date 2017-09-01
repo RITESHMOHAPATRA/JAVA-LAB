92da239dd9c32fbf2a099bef5227f9c951dbde61
RITESHMOHAPATRA <mohapatraritesh97@gmail.com>
2017-09-01 23:13:30 +0530

    WAP to remove HTML tag from the string.

---
+++ p77.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class HtmlTagRemover {
+ 
+    public static void main(String a[])
+    {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println(" enter text with html tags");
+        String text = sc.nextLine();
+        text = text.replaceAll("\\<.*?\\>", "");
+        System.out.println("text  without tags = "+text+"\n");
+    }
+}
