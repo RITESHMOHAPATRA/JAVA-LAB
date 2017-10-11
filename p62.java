33998040cf29cbc8e77b4f5263b1b893a8c45586
RITESHMOHAPTRA <mohapatraritesh97@gmail.com>
2017-10-11 19:31:26 +0530

    Write a Program in Java to input a number in Decimal number system and convert it into

--- p62.java
+++ p62.java
@@ -5,12 +5,13 @@ class DecimalToBinary
     {
         int n,a;
         Scanner s = new Scanner(System.in);
+        String x="";
         System.out.print("Enter any decimal number:");
         n = s.nextInt();
         while(n > 0)
         {
             a = n % 2;
-            x = x + "" + a;
+            x = a + "" + x;
             n = n / 2;
         }
         System.out.println("Binary number: "+x);
